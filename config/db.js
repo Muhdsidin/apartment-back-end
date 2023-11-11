1bf6130d3edccdf4bbde4cde2668da30f6540431
sidin <officialzedro@gmail.com>
2023-11-11 23:04:22 +0530

    git commiting

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-const mongooose = require("mongoose")
-const URL = process.env.MONGO_DB_URL ||'mongodb+srv://officialzedro:booking@sidin.9mqzofi.mongodb.net/rooms' 
-const connectDatabase = async()=>{
-    try {
-        mongooose.connect(URL)
-        console.log("server is conected")
-        
-    } catch (error) {
-        console.log(`error${error}`)
-    }
+const mongoose = require("mongoose");
+const URL = process.env.MONGO_DB_URL || 'mongodb+srv://bsidin79:booking@zedro.josllbg.mongodb.net/?retryWrites=true&w=majority';
 
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Server is connected to MongoDB");
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error}`);
+    }
 }
 
-module.exports = connectDatabase
-
-/*"mongodb+srv://officialzedro:rooms@sidin.9mqzofi.mongodb.net/"*/ 
+module.exports = connectDatabase;
