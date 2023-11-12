0167a9cd4953cfa54c8c9c45a8ffaed6885bac35
sidin <officialzedro@gmail.com>
2023-11-12 12:23:00 +0530

    hello world

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,53 +1,51 @@
-const express = require("express")
-const router = express.Router()
-const multer = require("multer")
-const ProductModel = require("../models/Product-Model")
-const cloudinary = require("cloudinary").v2
-const upload = multer({dest:"upload/"})
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const ProductModel = require("../models/Product-Model");
+const cloudinary = require("cloudinary").v2;
+
+const upload = multer({ dest: "upload/" });
+
 cloudinary.config({
-    cloud_name: 'desvgqarv',
-    api_key: '145346287342629',
-    api_secret: 'QKV7NMERDvVo5V9IOr1A0sR51yg',
-})
+  cloud_name: 'desvgqarv',
+  api_key: '145346287342629',
+  api_secret: 'QKV7NMERDvVo5V9IOr1A0sR51yg',
+});
 
-router.get("/upload",upload.array("images"),async(req,res)=>{
-    try {
-        
-    const {title , prize , location } = req.query
-    const imageOne = req.files[0];
-    const imageTwo = req.files[1];
-    const imageThree = req.files[2];
+router.post("/upload", upload.array("images"), async (req, res) => {
+  try {
+    const { title, prize, location } = req.query;
+    const [imageOne, imageTwo, imageThree] = req.files;
 
-    const uploadImageOne = await cloudinary.uploader.upload(imageOne.path,{
-        folder:"rooms"
-    })
+    const uploadImageOne = await cloudinary.uploader.upload(imageOne.path, {
+      folder: "rooms",
+    });
 
-    const uploadImagTwo = await cloudinary.uploader.upload(imageTwo.path,{
-        folder:"rooms"
-    })
+    const uploadImageTwo = await cloudinary.uploader.upload(imageTwo.path, {
+      folder: "rooms",
+    });
 
-    const uploadImageThree = await cloudinary.uploader.upload(imageThree.path,{
-        folder:"rooms"
-    })
+    const uploadImageThree = await cloudinary.uploader.upload(imageThree.path, {
+      folder: "rooms",
+    });
 
     const uploadToDatabase = await ProductModel.create({
-        title,
-        prize,
-        location,
-        imageUrl: [uploadImagTwo,uploadImageOne,uploadImageThree]
-    })
+      title,
+      prize,
+      location,
+      imageUrl: [uploadImageTwo, uploadImageOne, uploadImageThree],
+    });
 
     res.status(200).json({
-        message:"you SuccessFully Uploaded "
-    })
-        
-    } catch (error) {
-        res.status(401).json({
-            message: `error: {
-                ${error}
-            }`
-        })
-    }
-})
+      message: "Successfully Uploaded",
+    });
+  } catch (error) {
+    console.error("Error in upload route:", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
