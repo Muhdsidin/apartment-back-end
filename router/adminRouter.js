8b67732d8d952e6b12878773b3bd53d58144ee1e
sidin <officialzedro@gmail.com>
2023-11-12 12:27:04 +0530

    hello world

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -4,36 +4,34 @@ const multer = require("multer");
 const ProductModel = require("../models/Product-Model");
 const cloudinary = require("cloudinary").v2;
 
-const upload = multer({ dest: "upload/" });
-
 cloudinary.config({
   cloud_name: 'desvgqarv',
   api_key: '145346287342629',
   api_secret: 'QKV7NMERDvVo5V9IOr1A0sR51yg',
 });
 
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+});
+
 router.post("/upload", upload.array("images"), async (req, res) => {
   try {
     const { title, prize, location } = req.query;
-    const [imageOne, imageTwo, imageThree] = req.files;
-
-    const uploadImageOne = await cloudinary.uploader.upload(imageOne.path, {
-      folder: "rooms",
+    const uploadPromises = req.files.map(async (file) => {
+      const result = await cloudinary.uploader.upload(file.buffer, {
+        folder: "rooms",
+      });
+      return result.secure_url;
     });
 
-    const uploadImageTwo = await cloudinary.uploader.upload(imageTwo.path, {
-      folder: "rooms",
-    });
-
-    const uploadImageThree = await cloudinary.uploader.upload(imageThree.path, {
-      folder: "rooms",
-    });
+    const imageUrls = await Promise.all(uploadPromises);
 
     const uploadToDatabase = await ProductModel.create({
       title,
       prize,
       location,
-      imageUrl: [uploadImageTwo, uploadImageOne, uploadImageThree],
+      imageUrl: imageUrls,
     });
 
     res.status(200).json({
