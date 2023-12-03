7c95c3b5a987395dfc295a6fad3d14fc17000be2
sidin <officialzedro@gmail.com>
2023-12-03 23:14:15 +0530

    last change

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -39,9 +39,6 @@ router.post("/upload-building", async(req,res)=>{
   res.status(200).json({
     message:"SuccussFully Uploaded"
   })
-   res.status(200).json({
-     message:"Uploaded"
-   })
  } catch (error) {
   console.log(error)
   res.status(404).json({
